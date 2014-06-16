7575d7771da482946fba62e32d7734480d561ed8
Clint Liddick <clint.liddick@gmail.com>
2014-06-16 14:38:54 -0400

    main class w/ test setup

--- assig3/src/main/java/assig3/LZWmod.java
+++ assig3/src/main/java/assig3/LZWmod.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 public class LZWmod {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
     System.out.println("working");
     final Logger logger = LoggerFactory.getLogger(LZWmod.class);
     logger.info("logger working");
---
+++ assig3/src/test/java/assig3/LZWmodTest.java
@@ -0,0 +1,15 @@
+package assig3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class LZWmodTest {
+
+  @Ignore
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
